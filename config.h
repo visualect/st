7cfdcd214a4d7ceb61b182385cd15f99aac0627f
ian <yanikidev@gmail.com>
2025-06-08 20:55:15 +0200

    change font size

--- config.h
+++ config.h
@@ -7,13 +7,16 @@
  */
 // static char *font = "Liberation
 // Mono:pixelsize=12:antialias=true:autohint=true";
+//
 static char *font =
     "JetBrainsMono Nerd Font:pixelsize=16:antialias=true:autohint=true";
+
 /* Spare fonts */
 static char *font2[] = {
-    /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true",
+    /*	"Inconsolata for
+     * Powerline:pixelsize=12:antialias=true:autohint=true",
      */
-    "Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true",
+    "Hack Nerd Font Mono:pixelsize=16:antialias=true:autohint=true",
 };
 
 static int borderpx = 2;
